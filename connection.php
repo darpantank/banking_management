351c8fbc2cdd87515b206e340a7202ec51eb4f88
darpantank <105478582+darpantank@users.noreply.github.com>
2022-05-21 23:34:47 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php 
+
+$server="localhost";
+$username="root";
+$password="";
+$db="basic_banking_system_database";
+
+$conn=mysqli_connect($server,$username,$password,$db);
+?>
---
+++ index.php
@@ -0,0 +1,79 @@
+
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+    <link rel="stylesheet" href="/css/first.css">
+
+    <title>Basic Banking System</title>
+    <style type="text/css">
+        button{
+            position: relative;
+    display: inline-block;
+    padding: 12px 36px;
+    margin: 10px 0;
+    color: #fff;
+    text-decoration: none;
+    text-transform: uppercase;
+    font-size: 18px;
+    letter-spacing: 2px;
+    border-radius: 8px;
+    background-color: #9dc5c3;
+    background-image: linear-gradient(315deg, #9dc5c3 0%, #5e5c5c 74%);
+        }
+    </style>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+</head>
+
+
+<body>
+    <?php
+  include 'navbar.php';
+  ?>
+
+    <div class="container-fluid">
+        <!-- Introduction section -->
+        <div class="intro py-1">
+            <div >
+                <div >
+                   <center> <h1 class="man"> Welcome to My Bank</h1></center>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md img text-center">
+            </div>
+        </div>
+        <!-- Activity section -->
+        <div class="row activity text-center">
+
+            <div class="col-md act">
+                <br><br>
+                <img src="images/moneytransfer.png" width=200px hight=20px class="img-fluid">
+                <br><br>
+                <a href="transfermoney.php"><button>Transfer Money</button></a>
+            </div>
+            <div class="col-md act">
+                <br><br>
+                <img src="images/history.png" width=200px hight=200px class="img-fluid" >
+                <br><br>
+                <a href="transactionhistory.php"><button > History</button></a>
+            </div>
+        </div>
+    </div>
+
+
+    <footer class="text-center mt-5 py-2">
+        <p>&copy 2022. Made by <b>Darpankumar Tank</b> <br> For the Project of  <b>The Sparks Foundation</b></p>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+
+</html>
